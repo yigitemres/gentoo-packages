306da9a24aa3cba8047693734ba0b389d38af364
Yigit Emre Sahinoglu <yigitemres@gmail.com>
2020-03-20 21:25:02 +0300

    Add app-emacs/visual-regexp and app-emacs/visual-regexp-steroids.

---
+++ app-emacs/visual-regexp-steroids/files/99visual-regexp-steroids-gentoo.el
@@ -0,0 +1,2 @@
+(add-to-list 'load-path "@SITELISP@")
+(load "visual-regexp-steroids-autoloads.el" nil t)
---
+++ app-emacs/visual-regexp/files/99visual-regexp-gentoo.el
@@ -0,0 +1,2 @@
+(add-to-list 'load-path "@SITELISP@")
+(load "visual-regexp-autoloads.el" nil t)
